a1d5e61dad57a37fcb901adbb0ea6832e9f9eb7e
Noah Allen <allennoah368@gmail.com>
2022-02-08 16:43:13 -0700

    Capture form field values

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,27 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
-  var listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
-  tasksToDoEl.appendChild(listItemEl);
+  event.preventDefault();
+
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  console.dir(taskNameInput);
+
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  console.log(taskTypeInput);
+
+  var listItemEL= document.createElement("li");
+  listItemEL.className = "task-item";
+
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+
+  taskInfoEl.innerHTML = "<h3 class ='task-name'>" + taskNameInput + "</h3><span class ='task-type'>" + taskTypeInput + "</span>";
+
+  listItemEL.appendChild(taskInfoEl);
+
+  tasksToDoEl.appendChild(listItemEL);
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
   
